823b54e4ed6a938c9736494e43e01d5b6f47371b
Elvis Nuñez <elvisnunez@me.com>
2016-07-01 09:47:28 +0200

    Skeleton project

---
+++ Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project
+//
+//  Created by Elvis Nuñez on 01/07/16.
+//  Copyright © 2016 3lvis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Elvis Nuñez on 01/07/16.
+//  Copyright © 2016 3lvis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
